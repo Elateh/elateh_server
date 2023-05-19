8344f7a4dc8f7be76b9b93cbc340c5583c990106
Yarema87 <119173438+Yarema87@users.noreply.github.com>
2023-05-19 19:42:21 +0300

    Update controllers.py

--- mod_authentification/controllers.py
+++ mod_authentification/controllers.py
@@ -2,10 +2,10 @@ from flask import Blueprint, jsonify, request
 
 from app.mod_authentification.models import Users
 
-mod_healthcheck = Blueprint('authentification', __name__, url_prefix='/authentification')
+mod_authentification = Blueprint('authentification', __name__, url_prefix='/authentification')
 
 @mod_authentification.route('/authentification', methods=['POST'])
-@mod_hauthentification.route('/', methods=['POST'])
+@mod_authentification.route('/', methods=['POST'])
 def addUser():
     data = [{
         'email': request.email,
