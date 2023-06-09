810e79135f414fe5763916ac321ee0458843d832
Yarema87 <119173438+Yarema87@users.noreply.github.com>
2023-06-09 19:18:44 +0300

    Update controllers.py

--- app/mod_cafe/controllers.py
+++ app/mod_cafe/controllers.py
@@ -1,9 +1,11 @@
 import json
 
+import app
 from flask import Blueprint, jsonify, request
 from sqlalchemy import exists
 
-from app.mod_cafe.models import Users
+from app.mod_cafe.models import Cafes
+from app.mode_dishes.models import Dishes
 from app import db
 
 mod_authentification = Blueprint('cafe', __name__, url_prefix='/cafe')
@@ -18,10 +20,17 @@ def after_request(response):
 def return_cafe():
     return jsonify({
         'picture': picture,
-        'name': name
+        'name': name,
         'dishes': [dish for dishes in types for types in dish_types]
     })
 
 def index():
-    cafe1 = Cafes(image='фішка.jpeg',name='Фішка',dishes=dish_types1)
-    })
+    with app.app_context():
+        dish_types1 = {'Лаваші': dish_list1, 'Хот-доги': dish_list2, 'Напої': dish_list3}
+        dish_list1 = [dish111, dish112, dish113]
+        dish_list2 = [Dishes()]
+        dish_list3 = [Dishes()]
+        group = Group(name='Group 1', dishes=dish_types)
+        cafe1 = Cafes(image='фішка.jpeg',name='Фішка',dishes=dish_types1)
+        db.session.add(cafe1)
+        db.session.commit()
