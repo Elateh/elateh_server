e3658aaef5682040f6d033dd4b8e628f9342ff3c
Yarema87 <119173438+Yarema87@users.noreply.github.com>
2023-06-13 15:15:33 +0300

    Update models.py

--- app/mod_order/models.py
+++ app/mod_order/models.py
@@ -2,9 +2,9 @@ from app import db
 from sqlalchemy import ForeignKey
 
 class Order(db.Model):
-    __tablename__ = 'cafes'
+    __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    cafe.id = db.Column(db.Integer, ForeignKey('cafes.id'))
-    dish.id = db.Column(db.Integer, ForeignKey('dishes.id'))
-    user.id = db.Column(db.Integer, ForeignKey('users.id'))
+    cafe_id = db.Column(db.Integer, ForeignKey('cafes.id'))
+    dish_id = db.Column(db.JSON)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
     date = db.Column(db.Datetime)
