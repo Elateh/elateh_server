0bacb4b597d513d358e7ebcfe517fdfe3513ac65
Yarema87 <119173438+Yarema87@users.noreply.github.com>
2023-06-13 15:03:22 +0300

    Update models.py

--- app/mod_order/models.py
+++ app/mod_order/models.py
@@ -6,3 +6,5 @@ class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     cafe.id = db.Column(db.Integer, ForeignKey('cafes.id'))
     dish.id = db.Column(db.Integer, ForeignKey('dishes.id'))
+    user.id = db.Column(db.Integer, ForeignKey('users.id'))
+    date = db.Column(db.Datetime)
