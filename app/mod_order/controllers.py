de759233aff88e54e65ddf7788520578fa365991
Yarema87 <119173438+Yarema87@users.noreply.github.com>
2023-06-14 23:23:50 +0300

    Update controllers.py

--- app/mod_order/controllers.py
+++ app/mod_order/controllers.py
@@ -26,6 +26,7 @@ def make_order():
     db.session.add(model)
     db.session.commit()
 
+    quantity = Order.query.count()
     id = model.id
     return jsonify({
         'id': id,
@@ -33,5 +34,34 @@ def make_order():
         'dish_id': dish_id,
         'type_id': type_id,
         'user_id': user_id,
-        'date': date
+        'date': date,
+        'quantity': quantity
     })
+
+@mod_order.route('/order/<int:order_id>/<int:type_id>/<int:cafe_id>/<int:dish_id>', method=['POST'])
+def remove_order(order_id, type1_id, cafe1_id, dish1_id):
+    query = Order.query.filter_by(id=order_id, type_id=type1_id, cafe_id=cafe1_id, dish_id=dish1_id).first()
+    db.session.delete(query)
+    db.session.commit()
+    quantity = Order.query.count()
+    data = [{
+        'id': order1_id,
+        'cafe_id': cafe_id,
+        'dish_id': dish_id,
+        'type_id': type_id,
+        'user_id': user_id,
+        'date': date
+    } for order1 in Order.query.all()]
+    return jsonify(data), quantity
+
+@mod_order.route('/order/<int:order_id>/<int:type_id>', method=['POST'])
+def check_order(order_id, type1_id):
+    query = Order.query.filter_by(id=order_id, type_id=type1_id).first()
+    if query:
+        return jsonify({
+            'success': False
+        })
+    else:
+        return jsonify({
+            'success': True
+        })
